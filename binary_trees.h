0f056ac5684ad0707a3cf2716583ae392f306cf1
Aladarahman <aladarahman18@gmail.com>
2024-01-03 05:55:27 -0800

    101-binary_tree_levelorder.c

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* advanced task */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif
